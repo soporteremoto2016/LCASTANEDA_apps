ccbfedf57e527fbe07b1d436d923d07060f601ef
Luc@s <soporteremoto2016@gmail.com>
2026-03-30 19:40:38 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -41,10 +41,10 @@ with st.sidebar:
 # Contenido principal (esto se mantendrá con los colores por defecto)
 st.title("Apliaciones IA ")
 
-url_ia="https://sites.google.com/view/aplicacionesdeia/inicio"
-st.subheader("En el siguiente enlace puedes encontrar páginas y ejercicios prácticos")
+#url_ia="https://sites.google.com/view/aplicacionesdeia/inicio"
+#st.subheader("En el siguiente enlace puedes encontrar páginas y ejercicios prácticos")
 
-st.write(f"Enlace para páginas y ejercicios: [Enlace]({url_ia})")
+st.write(f"Pruebas de Concepto apliacdas a IA : [Enlace]({url_ia})")
 col1, col2, col3 = st.columns(3)
 
 with col1:
